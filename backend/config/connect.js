151cb5a2bfa72952c7dff50ae2fbddc89064db96
HAMA <mohamed.ayarii1507@gmail.com>
2026-06-05 21:59:31 +0100

    Setup development environment and improve project structure

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/studentmangment')
-.then(()=>{
-    console.log("connected");
-}).catch((err)=>{
-    console.log(err);
-});
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
 
-module.exports = mongoose;
+        console.log("MongoDB Connected Successfully");
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
     const express = require("express");
     const app = express();
     const cors = require('cors');
-    
-    require("./config/connect");
+    require("dotenv").config();
 
+    
+    const connectDB = require("./config/connect");
+    connectDB();
+    
     app.use(express.json());
     app.use(cors());    
     
@@ -21,6 +24,6 @@
 
 
 
-    app.listen(3000,()=>{
-        console.log("server work on port 3000");
+    app.listen(process.env.PORT,()=>{
+        console.log("Server is running on port", process.env.PORT || 3000);
     });
